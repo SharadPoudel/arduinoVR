39bf7c474ce906e26ad75f92788fd0c81a13f81a
SharadPoudel <sharadpoudel.info@gmail.com>
2022-02-25 14:30:59 +0100

    updaate

--- arduino_pressurepads/arduino_pressurepads.ino
+++ arduino_pressurepads/arduino_pressurepads.ino
@@ -1,6 +1,7 @@
 #define FORCE_SENSOR_PIN1 A0 
 #define FORCE_SENSOR_PIN2 A1 
 #define FORCE_SENSOR_PIN3 A2 
+#define FORCE_SENSOR_PIN4 A3
 
 unsigned long last_time = 0;
 
@@ -15,8 +16,9 @@ void loop()
    int analogReading1 = analogRead(FORCE_SENSOR_PIN1);
    int analogReading2 = analogRead(FORCE_SENSOR_PIN2);
    int analogReading3 = analogRead(FORCE_SENSOR_PIN3);
+   int analogReading4 = analogRead(FORCE_SENSOR_PIN4);
 
-   if (analogReading1 > 5)
+   /*if (analogReading1 > 5)
      Serial.println("Sensor1_On");
    else
      Serial.println("Sensor1_Off");
@@ -27,7 +29,11 @@ void loop()
    if (analogReading3 > 5)     
      Serial.println("Sensor3_On");
    else
-     Serial.println("Sensor3_Off");
+     Serial.println("Sensor3_Off");*/
+   if (analogReading4 > 5)     
+     Serial.println("Sensor4_On");
+   else
+     Serial.println("Sensor4_Off");
 
    
    delay(16);
