7f4a4abc3c860b9045deb0aa73a916e73895ef68
Johan Ekman <37696604+MrShtekman@users.noreply.github.com>
2022-03-01 16:58:55 +0100

    test

--- RapidVRProto/Assets/Scripts/GameController.cs
+++ RapidVRProto/Assets/Scripts/GameController.cs
@@ -7,18 +7,21 @@ public class GameController : MonoBehaviour
 {
     [SerializeField] private float startDelay = 2;
     [SerializeField] private int bpm;
+    [SerializeField] AudioSource audioSource;
     public NoteInfo[] noteList;
 
 
     void Start()
     {
-
+        audioSource = gameObject.GetComponent<AudioSource>();
+        audioSource.PlayDelayed(startDelay);
     }
 
 
     void Update()
     {
 
+        //audioSource.PlayDelayed(startDelay);
     }
 }
 
--- RapidVRProto/Assets/Scripts/JohanTestScript/Note.cs
+++ RapidVRProto/Assets/Scripts/JohanTestScript/Note.cs
@@ -8,15 +8,34 @@ public class Note : MonoBehaviour
     [SerializeField] private bool pressable;
     void Start()
     {
-        
+
     }
 
-    
+
     void Update()
     {
         Debug.Log(Time.deltaTime);
         transform.Translate(0, 0, -speed * Time.deltaTime);
     }
 
+    /*private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Button"))
+            pressable = true;
 
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Button"))
+            pressable = false;
+    }*/
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Button") && other.GetComponent<Button>().buttonDown)
+        {
+            Destroy(gameObject);
+        }
+    }
 }
